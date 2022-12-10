6fc983995cdb1794e8c1b2de0d7b373434aa7ed9
Desmends27 <desmondmends27@gmail.com>
2022-12-10 22:26:41 +0000

    changed positions of , and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,11 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		if(i == 9)
+		if (i == 9)
 			continue;
 		else
-			putchar(' ');
+			putchar(',');
+		putchar(' ');
 	}
 	return (0);
 }
