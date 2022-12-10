edb3bf53be6e1360b6ff0be1b9696dcd417415ea
Desmends27 <desmondmends27@gmail.com>
2022-12-10 00:15:14 +0000

    removed variable last_num

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,25 +5,20 @@
 /**
  * main - check value of last number
  *
- * @num:last number
  * Return: 0 always
 */
 int main(void)
 {
 	int n;
-	int num;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	num = n % 10;
-
-	if (num > 5)
-		printf("Last digit of %d is %d and is greater than 5", n, num);
-	else if (num == 0)
-		printf("Last digit of %d is %d and is 0 ", n, num);
+	if (n % 10 > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, n % 10);
+	else if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0", n, n % 10);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0 ", n, num);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, n % 10);
 	return (0);
 }
 
