484abcfa283048e641f7cfc3b183321f1601f587
Desmends27 <desmondmends27@gmail.com>
2022-12-10 22:16:22 +0000

    changed everything about it

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,30 +1,24 @@
-#include <stdio.h>
+#include < stdio.h>
 /**
- * main - print all possible combinations of single digit numbers
- * @i - stores first number
- * @j - second number
- * @k - third number
- * TODO:try using only one variable
- * Return: 0 always (success)
+ * main - prints numbers in ascending order
+ * @i - first initializer
+ * @j - second initializer
+ *
+ * Return: 0 always(sucess)
 */
 int main(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 9; i++)
-{
-	for (j = 0; j < pow(10, i); j++)
+	for (int i = 0; i < 10; i++)
 	{
-	int k;
-		for (k = 0; k < i; k++)
+		for (int j = 0; j < 10; j++)
 		{
-			putchar('0' + j / pow(10, k) % 10);
+			putchar(i + '0');
+			putchar(j + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n');
-	}
 	return (0);
 }
-
