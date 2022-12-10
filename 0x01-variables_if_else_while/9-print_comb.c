f3572423a36910afa158df00e1cef57b9ae20910
Desmends27 <desmondmends27@gmail.com>
2022-12-10 14:34:20 +0000

    removed second loop not necessay

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,34 +2,23 @@
 /**
  * main - print all possible combinations of single digit numbers
  * @l - stores first number
- * @c - stores last number
  * TODO:try using only one variable
  * Return: 0 always (success)
 */
 int main(void)
 {
 	int l = 0;
-	int counter = 0;
-	int c  = 9;
 
 	while (l < 10)
 	{
 		putchar((l % 10) + '0');
 		if (l == 9)
 		{
+			putchar (' ');
 			break;
 		}
 		putchar(',');
 		putchar(' ');
-		if (counter)
-		{
-			while (c <= 0)
-			{
-				putchar((l % 10) + '0');
-				c--;
-			}
-		}
-		counter = 1;
 		l++;
 	}
 	return (0);
