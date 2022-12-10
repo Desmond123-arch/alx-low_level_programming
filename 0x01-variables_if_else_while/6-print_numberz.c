3b13bcf40611d2c8f66a3f86efa3fbf382860a17
Desmends27 <desmondmends27@gmail.com>
2022-12-10 13:22:57 +0000

    used putchar to print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+/**
+ * main - prints all numbers in base 10 using putchar
+ * @l - stores first number
+*/
+int main(void)
+{
+	int l = 0;
+
+	while (l < 10)
+	{
+		putchar((l%10) + '0');
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
