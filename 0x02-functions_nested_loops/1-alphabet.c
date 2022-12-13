263a260af66c3f8778bcdbe5218709e8a849f24f
Desmends27 <desmondmends27@gmail.com>
2022-12-13 23:56:01 +0000

    prints all alphabets in lowercasr

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ * @a - stores first letter
+ *
+ * Return: void
+*/
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	return;
+}
