2363a3377d53be865304aacd0bc2b863990fdcdb
Desmends27 <desmondmends27@gmail.com>
2022-12-14 23:57:25 +0000

    betty error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,8 +7,7 @@ void times_table(void)
 {
 	int i = 0;
 	int  j = 0;
-	int temp;
-	int product;
+	int temp, product;
 
 	while (i < 10)
 	{
@@ -16,26 +15,28 @@ void times_table(void)
 		{
 			product = i * j;
 			temp = product;
-
 			if (product > 9)
 			{
 				temp = product / 10;
 				_putchar(temp + '0');
 				temp = product % 10;
 				_putchar(temp + '0');
-
-
+				if (j != 9)
+				{
+				_putchar(' ');
+				_putchar(',');
+				}
 			}
 			else
 			{
 
 				_putchar(product + '0');
-				_putchar(' ');
-			}
-			if (j != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 			}
 			j++;
 		}
