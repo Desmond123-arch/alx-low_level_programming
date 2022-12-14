ebb5182870b9ebdf6257b51948191b5910670abf
Desmends27 <desmondmends27@gmail.com>
2022-12-14 21:49:45 +0000

    print last digit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,23 +2,42 @@
 /**
  * _abs - prints absolute value of a number
  * @n: function parameter
+ * @i: loop  parameter
+ * @counter: stores number of digits
+ * @temp: temporary stores n
+ * @ array : stores digit for printing
  * Return: 0(success)
 */
 int _abs(int n)
 {
+	int counter = 0;
+	int temp = n;
+
 	if (n < 0)
 		n = -n;
-	if (n == 0)
+	else
 	{
-		putchar('0');
-		return (0);
-	}
+		while (temp > 0)
+		{
+			temp /= 10;
+			counter++;
+		}
 
-	while (n != 0)
-	{	
-		n %= 10;
-		_putchar(n + '0');
-		n /= 10;
+		int array[counter];
+		int i = 0;
+
+		while (n > 0)
+		{
+			temp = n % 10; 
+			array[i] = temp;
+			n /= 10;
+			i++;
+		}
+		while (i)
+		{
+			_putchar (array[i] + '0');
+			i--;
+		}	
 
 	}
 	_putchar('\n');
