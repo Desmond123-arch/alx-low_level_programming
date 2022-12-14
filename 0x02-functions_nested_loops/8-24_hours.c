6619fd8306a44f529b3f65acb2bbef8641167804
Desmends27 <desmondmends27@gmail.com>
2022-12-14 23:09:58 +0000

    passed betty test

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * @i: first intializer
- * @j: second intializer
- * @temp: temporary value
+ *
+ *
 */
 void jack_bauer(void)
 {
-	int i,j;
+	int i, j;
 	int temp;
+
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
@@ -21,16 +21,16 @@ void jack_bauer(void)
 
 		       _putchar(temp + '0');
 		       _putchar(':');
-	       		
+
 			temp = j / 10;
-	 		
+
 			_putchar(temp + '0');
-			
+
 			temp = j % 10;
-			
+
 			_putchar(temp + '0');
 			_putchar('\n');
 		}
 	}
 
-}	
+}
