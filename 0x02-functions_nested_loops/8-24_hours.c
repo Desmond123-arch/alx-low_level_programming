5fc05a6a0555504271ed777cae359715e8546d9a
Desmends27 <desmondmends27@gmail.com>
2022-12-14 23:07:03 +0000

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * @i: first intializer
+ * @j: second intializer
+ * @temp: temporary value
+*/
+void jack_bauer(void)
+{
+	int i,j;
+	int temp;
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			temp = i / 10;
+
+			_putchar(temp + '0');
+
+			temp = i % 10;
+
+		       _putchar(temp + '0');
+		       _putchar(':');
+	       		
+			temp = j / 10;
+	 		
+			_putchar(temp + '0');
+			
+			temp = j % 10;
+			
+			_putchar(temp + '0');
+			_putchar('\n');
+		}
+	}
+
+}	
