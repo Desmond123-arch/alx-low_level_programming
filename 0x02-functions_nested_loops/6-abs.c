b5aded7c70ad0fce2e54a08050e4bf9aa2685871
Desmends27 <desmondmends27@gmail.com>
2022-12-14 00:55:11 +0000

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of a number
+ * @n: function parameter
+ *
+ * Return: 0(success)
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -(n);
+		_putchar(n + '0');
+	}
+	else
+		_putchar(n + '0');
+	return (0);
+}
+
