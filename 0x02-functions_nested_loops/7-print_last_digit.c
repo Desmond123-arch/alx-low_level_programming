612d645cedd1f1e7125cdcb698402e82730da69e
Desmends27 <desmondmends27@gmail.com>
2022-12-14 22:04:25 +0000

    added a new variable

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,21 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n:  function parameter
+ * @l: last digit
  * Return: value of last digit
 */
 int print_last_digit(int n)
 {
+	int l; 
+
 	if (n > 0)
 		n = -n;
 	if (n == 0)
 	{
 		return 0;
 	}
-	return (n % 10);
+	l = n % 10;
+	_putchar(l + '0');
+	return l;
+	
 }
