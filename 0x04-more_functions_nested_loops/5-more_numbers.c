bc8f999dcd6bc0256b42b6e1284c757489cbec8b
Desmends27 <desmondmends27@gmail.com>
2022-12-15 23:45:08 +0000

    print 0 to 14 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 to 14 10 times
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 15; i++)
+	{
+		if(i > 9)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
+
