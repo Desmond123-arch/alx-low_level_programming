ea2728d0af6ecf445d268428580c4e7d351f9528
Desmends27 <desmondmends27@gmail.com>
2022-12-16 01:41:49 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prints fizz buzz or fizzbuzz
+ *
+ * Return: 0 sucess
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		putchar(' ');
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
