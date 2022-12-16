c2ba4ff882048e6414bb90353e3c5a027c0821a8
Desmends27 <desmondmends27@gmail.com>
2022-12-16 01:23:56 +0000

    fixed betty error

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,7 +7,8 @@
 void print_square(int size)
 {
 	int i, j;
-	if ( size > 0)
+
+	if (size > 0)
 	{
 
 		for (i = 0; i < size; i++)
@@ -19,8 +20,6 @@ void print_square(int size)
 			_putchar('\n');
 		}
 	}
-	else if ( size == 0)
-		_putchar('\n');
 	else
 		_putchar('\n');
 }
