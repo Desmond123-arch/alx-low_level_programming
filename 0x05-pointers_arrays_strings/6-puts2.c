da08177996f358e9f09823857be373794fca98fc
Desmends27 <desmondmends27@gmail.com>
2022-12-20 00:55:31 +0000

    used strlen instead

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,18 @@
 #include "main.h"
-
+#include "2-strlen.c"
 /**
  * puts2 - prints every other character of a string
  * @str: pointer to first character
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int len = _strlen(str);
+
+	while (len >= 0)
 	{
 		_putchar(*str);
-		str++;
-		str++;
+		str = +2;
+		len++;
 	}
 	_putchar('\n');
 }
