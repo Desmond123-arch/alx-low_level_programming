b652081e6ffa524cd6c08ac5edb4292b7f6e0ba0
Desmends27 <desmondmends27@gmail.com>
2022-12-20 01:41:29 +0000

    print an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints an arrray
+ * @a: array name
+ * @n: array size
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i)
+		{
+		putchar(',');
+		}
+		putchar(' ');
+		printf("%d", *(a + i));
+	}
+	putchar('\n');
+}
