4ce59a8643bf0a170f4f426449ca17f28fa53231
Desmends27 <desmondmends27@gmail.com>
2022-12-22 00:30:41 +0000

    test

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,27 +12,17 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
 			continue;
-		else if (s1[i] > s2[i])
-		{
-			return (15);
-		}
 		else
-		{
-			return (-15);
-		}
+			if (s1[i] > s2[i])
+				return (15);
+			else
+				return (-15);
 
 	}
-	if (_strlen(s1) > _strlen(s2))
-	{
-		return (-15);
-	}
-	else if (_strlen(s2) > _strlen(s1))
-		return (15);
-	else
 		return (0);
 }
 
