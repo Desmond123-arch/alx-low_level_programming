adff452526af979ea02c8dac0215b97d2f10d397
Desmends27 <desmondmends27@gmail.com>
2022-12-22 16:56:21 +0000

    test

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,22 +9,22 @@
  * if not equal and s1_len > s2_len return positive value(15)
  * else if s2_len>s1_len return negative value
  */
-int strCmp(char string1[], char string2[] )
+int _strcmp(char *s1, char *s2)
 {
     int i = 0;
     int flag = 0;    
     while (flag == 0)
     {
-        if (string1[i] > string2[i])
+        if (s1[i] > s2[i])
         {
             flag = 1;
         }
-        else if (string1[i] < string2[i])
+        else if (s1[i] < s2[i])
         {
             flag = -1;
         }
 
-        if (string1[i] == '\0')
+        if (s2[i] == '\0')
         {
             break;
         }
