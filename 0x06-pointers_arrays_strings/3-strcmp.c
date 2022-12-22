a936cc2e53582ff341681d776477a600f303acc1
Desmends27 <desmondmends27@gmail.com>
2022-12-22 21:38:07 +0000

    rewrote the funtion

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include "strlen.c"
-#include "stddef.h"
 /**
  * _strcmp- compares two intergers to find if they are the same
  * @s1: pointer to first string
@@ -11,16 +9,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
-    	for (i = 0; ; i++)
-    	{
-        	if (s1[i] != s2[i])
-		       	return s1[i] < s2[i] ? -1 : 1;
-		else if (s1[i] == '\0' && s2[i] == '\0')
-			return 0;
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
+		i++;
 	}
-	if (s1[i] == '\0')
-		 return 0;
+	if (s1[i] > s2[i])
+		return (15);
+	else if (s2[i] > s1[i])
+		return (-15);
+	else
+		return (0);
 }
-
