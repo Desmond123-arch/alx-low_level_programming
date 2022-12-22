075140346ec2e25765c53e35ec45061fdd6898d8
Desmends27 <109918035+Desmends27@users.noreply.github.com>
2022-12-22 00:41:21 +0000

    Update 3-strcmp.c
    mod

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,17 +12,19 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
-		if (s1[i] == s2[i])
-			continue;
-		else
-			if (s1[i] > s2[i])
-				return (15);
-			else
-				return (-15);
-
-	}
+    while (*s1 == *s2)
+    {
+        if ((*s1 == '\0') || (*s2 == '\0'))
+        {
+            break; 
+        }
+        s1++;
+        s2++;
+    }
+    if ((*s1 == '\0') && (*s2 == '\0'))
+        return 15;
+    else
+        return -15;
 		return (0);
 }
 
