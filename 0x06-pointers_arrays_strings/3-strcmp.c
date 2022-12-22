ee09a244566dd4beb297d2bfda463a443cb54ba4
Desmends27 <desmondmends27@gmail.com>
2022-12-22 00:17:50 +0000

    test against checker

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
 			continue;
@@ -31,7 +31,7 @@ int _strcmp(char *s1, char *s2)
 		return (-15);
 	}
 	else if (_strlen(s2) > _strlen(s1))
-		return (15);
+		return (15)
 	else
 		return (0);
 }
