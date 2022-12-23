b10b41ab10890a6f835c29d994253000e10a29e4
Desmends27 <desmondmends27@gmail.com>
2022-12-23 18:38:07 +0000

    setj to zero again

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,17 +7,17 @@
  */
 char *leet(char *s)
 {
-	char *a = "AEOTL";
-	char *l = "aeotl";
-	char *c = "43071";
+	char a[] = "AaEeOoTtLl";
+	char c[] = "4433007711";
 	int i = 0;
 	int j = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 4; j++)
+		j = 0;
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (s[i] == a[j] || s[i] == l[j])
+			if (a[j] == s[i])
 			{
 				s[i] = c[j];
 			}
