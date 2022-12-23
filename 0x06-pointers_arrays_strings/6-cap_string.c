2eccfb6117355e69d177826b30463f1d2835001b
Desmends27 <desmondmends27@gmail.com>
2022-12-23 01:19:29 +0000

    extra help

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,29 @@
 #include "main.h"
-#include "5-string_toupper.c"
-#include <ctype.h>
 /*
  * cap_string - capitalizes the first letter in a string
  * @s: pointer to first char in string
  * Return: char pointer to string
  */
-char *cap_string(char *s)
+char *cap_string(char *string)
 {
 	int i = 0;
-	int flag = 0;
-
-	while (s[i] != '\0')
+	while (string[i] != '\0')
 	{
-		if (isspace(s[i]))
+		if (string[0] >= 97 && string[0] <= 122)
+				string[0] = string[0] - 32;
+		if (string[i] == ' ' || string[i] == '\t' || string[i] == '\n'
+		    || string[i] == ',' || string[i] == ';' || string[i] == '.'
+		    || string[i] == '.' || string[i] == '!' || string[i] == '?'
+		    || string[i] == '"' || string[i] == '(' || string[i] == ')'
+		    || string[i] == '{' || string[i] == '}')
 		{
-			flag = 1;
+			if(string[i + 1] >= 97  && string[i + 1] <= 122)
+				string[i + 1] = string[i + 1] - 32;
 		}
-		else if (flag)
-		{
-			s[i] = toupper(s[i]);
-			flag = 0;
-		}
-		i++;
+	      i++;
 	}
 
-
-	return (s);
+	return (string);
 }
 
 
