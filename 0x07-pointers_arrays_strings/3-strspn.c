7a81f12bd07e0264335c8e9fa9e23923e77c6172
Desmends27 <desmondmends27@gmail.com>
2023-01-04 16:35:21 +0000

    modified if statement

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,15 +3,15 @@
 /**
  * _strspn - gets the length of a prefix substring
  * @s: pointer to first char in string
- * @accept: pointer to second string being compared to 
- * Return:number of bytes in the initial segment 
+ * @accept: pointer to second string being compared to
+ * Return:number of bytes in the initial segment
  * of s which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int len = 0;
 	int found;
-	int i = 0; 
+	int i = 0;
 	int j = 0;
 
 	while (i <= _strlen(s) )
@@ -21,14 +21,10 @@ unsigned int _strspn(char *s, char *accept)
 		{
 			while(j <= _strlen(accept))
 			{
-				if (accept[j]== s[i])
+				if (accept[j] == s[i])
 				{
-					found = 1;
-				}
-				if (!found)
-					break;
-				else
 					len++;
+				}
 				j++;
 			}
 		}
