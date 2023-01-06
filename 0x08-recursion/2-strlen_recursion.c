a27bea9a7f399fbe198108a3f69f4b46b8f9b590
Desmends27 <desmondmends27@gmail.com>
2023-01-06 00:38:24 +0000

    string reverse

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to first character in string
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+		return (len);
+	return (_strlen_recursion(s + 1) + 1);
+	len++;
+}
