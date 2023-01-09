5fdb5eb0e957ae56b2939f538fab41cf665fa276
Desmends27 <desmondmends27@gmail.com>
2023-01-09 23:44:56 +0000

    still fixing

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,14 +10,15 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	if (!(argc == 2))
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
+	int result = a * b;
 
-	printf("%d\n", a * b);
+	printf("%d\n", result);
 	return (0);
 }
