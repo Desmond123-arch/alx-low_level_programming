f8a135954b879da62d16ab67e2c19fbbf0f0d90b
Desmends27 <desmondmends27@gmail.com>
2023-01-11 21:22:00 +0000

    optimized solution

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,6 +10,10 @@
 int main(int argc, char *argv[])
 {
 	int coins, cents = 0;
+	int i;
+
+	/*array to store values of coins*/
+	int value[5] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
@@ -17,37 +21,20 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	coins = atoi(argv[1]);
+
 	if (coins < 0)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	while (coins)
+	/*use a for loop to loop through array */
+	for (i = 0; i < 5 && coins != 0; i++)
 	{
-		if (coins >= 25)
+		/*divide and modulate to chage value of coins left */
+		if (value[i] <= coins)
 		{
-			cents += coins / 25;
-			coins = coins % 25;
-		}
-		if (coins >= 10)
-		{
-			cents += coins / 10;
-			coins = coins % 10;
-		}
-		if (coins >= 5)
-		{
-			cents += coins / 5;
-			coins = coins % 5;
-		}
-		if (coins >= 2)
-		{
-			cents += coins / 2;
-			coins = coins % 2;
-		}
-		if (coins >= 1)
-		{
-			cents += coins;
-			coins = coins % 1;
+			cents += coins / value[i];
+			coins = coins % value[i];
 		}
 	}
 	printf("%d\n", cents);
