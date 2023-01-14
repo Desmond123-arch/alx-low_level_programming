deeeb7fbb82841b89a0bfa4b744c1c5088f5bb9b
Desmends27 <desmondmends27@gmail.com>
2023-01-14 21:11:17 +0000

    test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > strlen(s2))
 		n = strlen(s2);
 
-	str = malloc(sizeof(s1) + n);
+	str = malloc(sizeof(s1) + n + 1);
 
 	if (str == NULL)
 		return (NULL);
