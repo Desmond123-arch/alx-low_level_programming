191971731da364d220f163897078ec6a57fd1899
Desmends27 <desmondmends27@gmail.com>
2023-01-14 19:45:04 +0000

    betty error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,10 +2,12 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b:set to 98
+ * Return: pointer to allocated space
+ * on error exits with argument 98
  */
 void *malloc_checked(unsigned int b)
 {
-	void  *ptr = malloc (b);
+	void  *ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
