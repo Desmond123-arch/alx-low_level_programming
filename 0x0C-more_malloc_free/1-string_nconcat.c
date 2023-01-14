bd4081890276a26ef695ac53d9af0f133d26bb9b
Desmends27 <desmondmends27@gmail.com>
2023-01-14 22:45:44 +0000

    used strlen + 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,17 +11,21 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-
+	/* check input cases */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n >= sizeof(s2))
-		n = sizeof(s2);
+	/*  used to account for null terminator*/
+	if (n >= strlen(s2))
+		n = strlen(s2) + 1;
 	str = malloc((strlen(s1) + n + 1) * sizeof(char));
+	/* check for value returned by malloc */
 	if (str == NULL)
 		return (NULL);
-	strcpy(str,s1);
-	strncat(str,s2,n);
+	/*use strcpy to copy s1 to str*/
+	strcpy(str, s1);
+	/*use strncat to concat n bytes of s2 to str*/
+	strncat(str, s2, n);
 	return (str);
 }
