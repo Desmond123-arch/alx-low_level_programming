c1d3180f5bf0e3616008f7b1835e04128186afae
Desmends27 <desmondmends27@gmail.com>
2023-01-16 22:27:53 +0000

    betty error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-
-	dog -> name = name;
-	dog -> age = age;
-	dog -> owner = owner;
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
 	return (dog);
 }
 
