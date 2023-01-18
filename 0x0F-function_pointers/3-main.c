ac3727c02cd623e3627f6865ad701d94307412b1
Desmends27 <desmondmends27@gmail.com>
2023-01-18 23:11:42 +0000

    incomplete

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,14 @@
+#include "3-calc.h"
+/**
+ * main - contains the main function
+ * @argc - argument counter
+ * @argv - argument vector
+ * Return: 1 success
+ */
+int main (__attribute__((unused))int argc, __attribute__((unused))char *argv[])
+{
+	/**
+	 * continue this tommorrow 
+	 */
+	return(0);
+}
