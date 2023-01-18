e10df6a7809bde306b38dac33aad01f85965c391
Desmends27 <desmondmends27@gmail.com>
2023-01-18 21:47:30 +0000

    compare members in an array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an interger
+ * @array : pointer to first member of array
+ * @size : size of array
+ * @cmp: function used for a comparision
+ * Return: index of found number
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	/* check for various guards for function */
+	if (array == NULL)
+		exit(0);
+	if (size <= 0)
+		return (-1);
+	/* iterate over each member and perform the action */
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
