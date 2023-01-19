6e48d9b40a2a0f56b110f904af245f52632717c3
Desmends27 <desmondmends27@gmail.com>
2023-01-19 15:18:05 +0000

    this is wrong

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,9 +17,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i  = 0;
 
-	while (i < 6)
+	while (i < 5)
 	{
-		if (s[0] == ops->op[i])
+		if (*s == ops->op[i])
 		{
 			break;
 		}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,10 +5,29 @@
  * @argv: argument vector
  * Return: 1 success
  */
-int main(__attribute__((unused))int argc, __attribute__((unused))char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
-	/**
-	 * continue this tommorrow
-	 */
+	int A,B;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	operation = get_op_func(argv[2]);
+	if (operation == NULL)
+	{
+		printf("Error");
+		exit(99);
+	}
+	A = atoi(argv[1]);
+	B = atoi(argv[3]);
+	printf("%d\n",operation(A,B));
 	return (0);
 }
