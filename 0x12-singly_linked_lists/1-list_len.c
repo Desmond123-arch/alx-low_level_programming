0a04fa02b09da013205730e8e0c542f24c031dee
Desmends27 <desmondmends27@gmail.com>
2023-01-26 23:25:45 +0000

    find the number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - find the number of elements in a linked list
+ * @h: pointer to the struct list_t
+ * Return: number of elements in list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	const list_t *tmp = h;
+
+	while (tmp != NULL)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	return (i);
+}
