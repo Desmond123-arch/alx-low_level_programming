086234e0e296328010ad8efa0c3663ed00660652
Desmends27 <desmondmends27@gmail.com>
2023-01-31 17:30:15 +0000

    check if head == NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *new = malloc(sizeof(listint_t));
 
-	if (new == NULL || *head == NULL)
+	if (new == NULL || head == NULL)
 	{
 		return (NULL);
 	}
