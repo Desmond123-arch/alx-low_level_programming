cd6d57bd33bc87064e6ce191d559d2e82beb71d5
Desmends27 <desmondmends27@gmail.com>
2023-02-03 13:25:14 +0000

    used bit shift

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include  "main.h"
-unsigned int _pow(unsigned int  num, unsigned int exp);
 /**
  * binary_to_uint - converts a binary number to an int
  * @b: b is pointing to a string of 0 and 1 chars
@@ -10,39 +9,21 @@ unsigned int _pow(unsigned int  num, unsigned int exp);
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0;
-	unsigned  long num = atoi(b);
+	unsigned int  num = 0;
 	unsigned int len = strlen(b);
-	unsigned int sum = 0;
 
-	if (b == NULL || num == 0)
+	if (b == NULL)
 	{
 		return (0);
 	}
+
 	for (i = 0; i < len; i++)
 	{
 		if (!(b[i] >= 48 && b[i] <= 58))
 		{
 			return (0);
 		}
-		sum += (num % 10) *  _pow(2, i);
-		num = num / 10;
+		num = (num << 1) | (b[i] - '0');
 	}
-	return (sum);
-}
-/**
- * _pow - return a number raised to a power
- * @num: number to be raised to power
- * @exp: power to be raised to
- * Return: power of number
- */
-unsigned int _pow(unsigned int  num, unsigned int exp)
-{
-	unsigned int product = 1;
-
-	while (exp)
-	{
-		product *= num;
-		exp--;
-	}
-	return (product);
+	return (num);
 }
