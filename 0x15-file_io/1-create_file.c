8322060b3ce682978048ee3d146974cb6e023ac1
Desmends27 <desmondmends27@gmail.com>
2023-02-07 13:55:30 +0000

    check for file with no read permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 	check = write(file, text_content, strlen(text_content));
 	if (check == -1)
 	{
-		close (file);
+		close(file);
 		return (-1);
 	}
 	close(file);
