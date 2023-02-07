cd9e1cf53f5332b46b8b74abe8e410bf3bb314ff
Desmends27 <desmondmends27@gmail.com>
2023-02-07 13:05:30 +0000

    v 2.0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,33 +8,24 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file = open(filename, O_RDONLY);
-	ssize_t i = 0, check = 0;
-	size_t j = 0;
-	char c[1];
+	ssize_t i = 0;
+	ssize_t j = 0;
+	char *buffer = malloc(sizeof(char) * letters);
 
+	if (buffer == NULL)
+		return (0);
 	if (filename == NULL)
-	{
 		return (0);
-	}
 	if (file == -1)
-	{
 		return (0);
-	}
-	while ((check = read(file, &c, 1)) > 0 && j < letters)
-	{
-		i += write(1, c, 1);
-		if (i == -1)
-		{
-			close(file);
-			return (0);
-		}
-		j++;
-	}
+	j = read(file, buffer, letters);
+	if (j == -1)
+		close(file);
+		return (0);
+	i += write(STDOUT_FILENO, buffer, j);
+	if (i == -1)
+		return (0);
 	close(file);
-	if (check == -1)
-	{
-		return (0);
-	}
 	return (i);
 }
 
