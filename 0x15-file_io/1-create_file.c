77d204db2c386efceca7627cd72bb2073a01408a
Desmends27 <desmondmends27@gmail.com>
2023-02-07 14:13:05 +0000

    check for null string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,15 +9,17 @@ int create_file(const char *filename, char *text_content)
 {
 	int file;
 	ssize_t check = 0;
-
+	int len = 0;
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-	check = write(file, text_content, strlen(text_content));
+	if (text_content != NULL)
+		len = strlen(text_content);
+	check = write(file, text_content, len);
 	if (check == -1)
 	{
 		close(file);
