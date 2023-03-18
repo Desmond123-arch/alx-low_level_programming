6c230ee768f5005128097f0757c3c0679fbbf2f8
Desmends27 <desmondmends27@gmail.com>
2023-03-18 03:17:21 +0000

    betty error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
-		return(add_dnodeint(h,n));
+		return (add_dnodeint(h, n));
 	}
 	for (i = 0; temp != NULL; temp = temp->next, i++)
 	{
@@ -42,7 +42,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (temp == NULL && i == idx)
 	{
-		return  (add_dnodeint_end(h, n));
+		return (add_dnodeint_end(h, n));
 	}
 	return (NULL);
 }
