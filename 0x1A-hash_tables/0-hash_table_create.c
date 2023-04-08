571b96fa2aba5bbca06ef8b4ccc064dc22201695
Desmends27 <desmondmends27@gmail.com>
2023-04-08 14:20:03 +0000

    pointer instead

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,10 +7,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	int i = 0;
+	hash_table_t *table;
 
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (size <= 0)
+		return NULL;
+
+	table = malloc (sizeof(hash_table_t));
+	if (table == NULL)
+		return NULL;
+
 	table->size = size;
-	table->array = malloc(table->size * sizeof(hash_node_t));
+	table->array = malloc(size * sizeof(hash_node_t *));
 
 	if (table == NULL)
 	{
