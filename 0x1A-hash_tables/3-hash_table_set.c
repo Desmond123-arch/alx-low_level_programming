21dacaec55885848e4dbbe939316569ebcd41d45
Desmends27 <desmondmends27@gmail.com>
2023-04-09 00:57:09 +0000

    malloc return

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,62 +11,40 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *current_item;
 	hash_node_t *item = malloc(sizeof(hash_node_t)); /*create an item*/
+	hash_node_t *temp;
 
 	if (strlen(key) == 0 || strlen(value) == 0)
-		return 0;
-
-	/* calculate index */
-	
-	index = key_index((const unsigned char *)key, ht->size);
-
-	/* set item */
-	item->key = malloc(sizeof(strlen(key) + 1));
+		return (0);
+	if (item == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);/*calculate index*/
+	item->key = malloc(sizeof(strlen(key) + 1)); /* set the item*/
 	item->value = malloc(sizeof(strlen(value) + 1));
 	item->key = strdup(key);
 	item->value = strdup(value);
 	item->next = NULL;
 	current_item = ht->array[index];
-
-	/*index is free*/
 	if (current_item == NULL)
 	{
-		/*insert into table */
 		ht->array[index] = item;
 		return (1);
 	}
 	else
 	{
-		/*Upadate the value */
-		if (strcmp(current_item->key, key) == 0)
+		if (strcmp(current_item->key, key) == 0) /*update the value*/
 		{
-			strcpy(ht->array[index]->value,value);
+			strcpy(ht->array[index]->value, value);
 			return (1);
 		}
-		else
+		else/*collisions*/
 		{
-			/*handle collisions*/
-			handle_collision(ht,index, item);
+			temp = table->array[index];
+			table->array[index] = item;
+			item->next = temp;
 			return (1);
 		}
-	
+
 	}
 	free_hash_table(ht);
 	return (0);
 }
-
-/**
- * handle_collision: takes car of collisions of nodes 
- * @table: hash table
- * @index: index of node with collision
- * @item: item to be added
- *  
- */
-
-void handle_collision(__attribute__((unused))hash_table_t *table, __attribute__((unused))unsigned long int index, __attribute__((unused))hash_node_t *item)
-{
-    /*list */
-    hash_node_t *temp = table->array[index];
-	table->array[index] = item;
-	item->next = temp;
-}
-
