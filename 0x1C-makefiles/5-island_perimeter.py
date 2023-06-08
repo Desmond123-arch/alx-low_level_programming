4e8b6b0c684d810a15dce86d78808d8f3b593fd6
Desmends27 <desmondmends27@gmail.com>
2023-06-08 15:33:39 +0000

    test02

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,28 +2,26 @@
 """ Calcuate the perimeter of a grid
 """
 def island_perimeter(grid):
-    """ Calcualtes the perimeter of a grid, each grid has 
-    a size of 1
-    """
-    row = len(grid)
-    col = len(grid[0])
-    perimeter = 4
-    total = 0
+    """ find the perimeter of a grid """
+    total_perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
 
-    for i in range(row):
-        perimeter = 4
-        for j in range(col):
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
-                if i == 0 or i == (row - 1) or j == 0 or j == (col - 1):
-                    perimeter -= 3
+                perimeter = 4
+                if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+                    perimeter -= 2
                 else:
                     if grid[i][j + 1] == 1:
                         perimeter -= 1
                     if grid[i][j - 1] == 1:
                         perimeter -= 1
-                    if grid[i+1][j] == 1:
+                    if grid[i + 1][j] == 1:
                         perimeter -= 1
-                    if grid[i-1][j] == 1:
+                    if grid[i - 1][j] == 1:
                         perimeter -= 1
-        total += perimeter
-    return total
+                total_perimeter += perimeter
+
+    return total_perimeter
