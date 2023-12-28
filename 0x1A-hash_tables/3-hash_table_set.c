b93e8e0df38e29d9645bdd6f2bb3aff25b3dda6c
Desmends27 <desmondopokumends2004@gmail.com>
2023-12-28 20:21:48 +0000

    fixed betty errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,24 +10,18 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index;
-	hash_node_t *current_item, *item;
-	hash_node_t *temp;
+	hash_node_t *current_item, *item, *temp;
 
 	if (ht == NULL || strlen(key) == 0 || strlen(value) == 0)
-	{
-		return 0;
-	}
+		return (0);
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
-	{
-		return 0;
-	}
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	item->key = strdup(key);
 	item->value = strdup(value);
 	item->next = NULL;
 	current_item = ht->array[index];
-
 	if (current_item == NULL)
 	{
 		ht->array[index] = item;
@@ -38,12 +32,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(current_item->key, key) == 0)
 		{
 			free(ht->array[index]->value);
-			current_item->value = strdup(value);
-			ht->array[index] = current_item;
+			ht->array[index]->value = strdup(value);
 			free(item->key);
 			free(item->value);
 			free(item);
-			return 1;
+			return (1);
 		}
 		else
 		{
